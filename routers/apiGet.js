4e11ad1396b9b8c7021f5926517f0da821b35f1a
Onel7d15h <Onelldish@hotmail.com>
2021-12-02 21:25:11 -0400

    Update

--- routers/apiGet.js
+++ routers/apiGet.js
@@ -6,6 +6,15 @@ router.get("/", (req, res) => {
     res.render("index");
 })
 
+//código que no conocía pero que es muy importante!
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 router.get("/apis", async (req, res) => {
     
     try 
